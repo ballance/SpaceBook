a8f39d9673939feeaf4c8b0b25d7f87258afc7ce
Chris Ballance <github@ballance.it>
2015-05-17 09:47:48 -0700

    Working on getting OWIN & Nancy serving up stuff

---
+++ SpacebookServer/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Hosting;
+
+namespace SpacebookServer
+{
+    public class Service
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://+:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Running on {0}", url);
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
